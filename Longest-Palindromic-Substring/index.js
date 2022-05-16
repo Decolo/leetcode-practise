1232672605580cfa2314fa4b64282502e25c9747
caicheng <caicheng@bytedance.com>
2022-05-16 11:23:16 +0800

    feat: longest palindromic string;

--- Longest-Palindromic-Substring/index.js
+++ Longest-Palindromic-Substring/index.js
@@ -1,47 +1,97 @@
-/**
- *
- * @param {string} s
- */
-var getReverse = function (s) {
-  let reverse = "";
-
-  for (let index = s.length - 1; index >= 0; index--) {
-    const element = s.charAt(index);
-    reverse += element;
+var isPalindromic = (s) => {
+  const len = s.length;
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (s.charAt(i) != s.charAt(len - i - 1)) {
+      return false;
+    }
   }
+  return true;
+};
+/** dynamic program */
 
-  return reverse;
+
+
+/**  center  */
+/**
+ * get palindromic length
+ * @param {string} s
+ * @param {number} left
+ * @param {number} right
+ * @returns
+ */
+const getPalindromicLen = function (s, left, right) {
+  let len = 0;
+
+  while (left >= 0 && right <= s.length - 1) {
+    if (s.charAt(left) === s.charAt(right)) {
+      len++;
+      left--;
+      right++;
+    } else {
+      break;
+    }
+  }
+  return {
+    len,
+  };
 };
 
-var getLSC = function (string1, string2) {
-  const table = [[], []];
-  for (let i = 0; i < string1.length; i++) {
-    for (let j = 0; j < string2.length; j++) {
-      if (i === 0 || j === 0) {
-        table[i][j] = 0;
-      } else if (string1.charAt(i) === string2.charAt(j)) {
-        table[i][j] = (table[i - 1] ? table[i - 1][j - 1] : 0) || 0;
-      } else {
-        table[i][j] = Math.max(
-          table[i][j - 1] || 0,
-          table[i - 1] ? table[i - 1][j] : 0
-        );
+
+var longestPalindrome = function (s) {
+  const len = s.length;
+  if (len === 1) {
+    return s;
+  }
+
+  if (len === 2) {
+    return isPalindromic(s) ? s : s.charAt(0);
+  }
+
+  let left = 0;
+  let right = 0;
+  let maxLen = 1;
+
+  for (let i = 0; i < len; i++) {
+    const { len: lenOdd } = getPalindromicLen(s, i - 1, i + 1);
+    const { len: lenEven } = getPalindromicLen(s, i, i + 1);
+    // debugger;
+    const finalLenOdd = lenOdd * 2 + 1;
+    const finalLenEven = lenEven * 2;
+
+    if (finalLenEven > finalLenOdd) {
+      if (finalLenEven > maxLen) {
+        // debugger
+        maxLen = finalLenEven;
+        left = Math.max(i - lenEven + 1, 0);
+        right = Math.min(i + lenEven, len - 1);
+      }
+    } else {
+      if (finalLenOdd > maxLen) {
+        // debugger
+        maxLen = finalLenOdd;
+        left = Math.max(i - lenOdd, 0);
+        right = Math.min(i + lenOdd, len - 1);
       }
     }
   }
 
-  debugger
-  return table;
+  return s.substring(left, right + 1);
 };
 
-/**
- * @param {string} s
- * @return {string}
- */
-var longestPalindrome = function (s) {
-  const reverseS = getReverse(s);
+// "babad"
+// expect: bab
+console.log(longestPalindrome("babad"));
 
-  getLSC(s, reverseS);
-};
+// "aacabdkacaa"
+// expect: aca
+console.log(longestPalindrome("aacabdkacaa"));
 
-longestPalindrome('abcba')
+// "aacdefcaa"
+// expect "aa"
+console.log(longestPalindrome("aacdefcaa"));
+
+// expect "bb"
+console.log(longestPalindrome("abcdbbfcba"));
+
+// expect "a"
+console.log(longestPalindrome("abcda"));
